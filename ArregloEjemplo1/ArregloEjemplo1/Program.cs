fb45ea57859ddf2ceefe5d2f3f9028ea37119873
jmmm204 <164056385+jmmm204@users.noreply.github.com>
2024-09-09 08:40:33 -0600

    Add files via upload

---
+++ ArregloEjemplo1/ArregloEjemplo1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArregloEjemplo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[10];
+            int contadorPar = 0, contadorImpar = 0;
+            Console.WriteLine("Digite los datos del arreglo");
+            Console.WriteLine();
+            for (int i = 0; i < 10; i++)
+            { // Lectura
+                Console.Write("Ingresa numero: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Datos del arreglo: ");
+            Console.WriteLine();
+            for (int i = 0;i < 10; i++)
+            { // Muestra en pantalla
+                Console.Write(numeros[i] + ", ");
+                if (numeros[i] % 2 == 0)
+                {
+                    contadorPar++;
+                }
+                else
+                {
+                    contadorImpar++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Cantidad de pares: " + contadorPar);
+            Console.WriteLine("Cantidad de impares: " + contadorImpar);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ArreglosEjemplo2/ArreglosEjemplo2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArreglosEjemplo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] tabla1 = new int[3, 3];
+            int suma = 0;
+            Console.WriteLine("Programa usando arreglos bidimensionales.");
+            Console.WriteLine("Digita los datos del arreglo:");
+
+            //Llenando el arreglo
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    tabla1[f, c] = int.Parse(Console.ReadLine());
+                    suma += tabla1[f, c];
+                }
+            }
+            // Imprimir el arreglo bidimensional
+            Console.WriteLine("Imprimiendo el arreglo... ");
+            for (int f = 0; f < 3; f++)
+            {
+                for (int c = 0; c < 3; c++)
+                {
+                    Console.Write(" " + tabla1[f, c]);
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("La suma total de los elementos del arreglo es: " + suma);
+            Console.ReadKey();
+        }
+    }
+}
